ca5065335914039aaa1285f468b33fc211753fe1
MarkoRatkovic94 <mareratkovic94@gmail.com>
2025-10-22 21:22:37 +0200

    Add files via upload

---
+++ JavaScript/Domaci 3 - If/domaci.js
@@ -0,0 +1,16 @@
+
+let name = "admin";
+let password = "tajnaSifra";
+
+if(name == "admin" && password == "tajnaSifra")
+{
+    console.log("Dobrodosli nazad");
+}
+else if (name == "Toma" && password == "tajnaSifra")
+{
+    console.log("Dobrodosao Tomo");
+}
+else
+{
+    console.log("Pogresna lozinka ili ime");
+}
